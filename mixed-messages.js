1a4ef5b3772d3246df741890ec48594eeff39cce
jesusantonio30 <morales.jesusant@gmail.com>
2025-01-21 21:29:14 -0800

    Added logic

--- mixed-messages.js
+++ mixed-messages.js
@@ -27,5 +27,30 @@
     + Array will contain 10 elements from 0 - 9
     + Use Math.random * array.length to generate a random number between 0 and 9, wrap this using Math.floor to round down
     + Using the generated number, wrap it around [] to access the index
+    + Return random quote
+    + Call function
 
 */
+
+const mixedMessages = ["'If you don't take risk, you can't create a future.' - Monkey D. Luffy", 
+                       "'No matter how hard or impossible it is, never lose sight of your goal.' - Monkey D. Luffy",
+                       "'What good are your happy ideals if you can't do anything to make them a reality?' - Crocodile",
+                       "'You can't see the whole picture until you look at it from the outside.' - Trafalgar Law",
+                       "'If you're too afraid of making mistakes you won't be able to do anything.' - Sanji",
+                       "'When the world shoves you around, you've just got to stand up and shove back. It's not like somebody's going to save you if you start babbling excuses!' - Roronoa Zoro",
+                       "'I devoted my entire life to becoming the World's Greatest Swordsman. I made my choice, so I'm the only one who gets to call me stupid.' - Roronoa Zoro",
+                       "'When you aim high, you often come across fights that just aren't worth fighting.' - Marshall D. Teach",
+                       "'If I give up now, I'm going to regret it.' - Monkey D. Luffy",
+                       "'I am going to be the Pirate King!' - Monkey D. Luffy"
+];
+
+
+const randomMessageGenerator = () => {
+
+    const randomMessage = mixedMessages[Math.floor(Math.random() * mixedMessages.length)];
+
+    return randomMessage;
+
+};
+
+console.log(randomMessageGenerator());
