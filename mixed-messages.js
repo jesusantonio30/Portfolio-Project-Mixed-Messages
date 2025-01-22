dae3ebd0326d371d4600970a212e03177b451311
jesusantonio30 <morales.jesusant@gmail.com>
2025-01-21 21:02:11 -0800

    Created a plan for project

---
+++ mixed-messages.js
@@ -0,0 +1,31 @@
+
+
+// # Plan Project - Mixed Messages
+
+/* 
+## What is the theme of this project? 
+    **One Piece Quotes!**
+
+## What quotes should be displayed?
+
+    1. "If you don't take risk, you can't create a future." - Monkey D. Luffy
+    2. “No matter how hard or impossible it is, never lose sight of your goal.” – Monkey D. Luffy
+    3. “What good are your happy ideals if you can’t do anything to make them a reality?” – Crocodile
+    4. “You can’t see the whole picture until you look at it from the outside.” – Trafalgar Law
+    5. "If you're too afraid of making mistakes you won't be able to do anything." - Sanji
+    6. "When the world shoves you around, you've just got to stand up and shove back. It's not like somebody's going to save you if you start babbling excuses!" - Roronoa Zoro
+    7. "I devoted my entire life to becoming the World's Greatest Swordsman. I made my choice, so I'm the only one who gets to call me stupid." - Roronoa Zoro
+    8. "When you aim high, you often come across fights that just aren't worth fighting." - Marshall D. Teach
+    9. "If I give up now, I'm going to regret it." - Monkey D. Luffy
+    10. "I am going to be the Pirate King!" - Monkey D. Luffy
+
+
+## Create a rough draft of how the program will operate.
+
+    1. Create a function that generates a random quote when called
+    + Create an array that stores quotes
+    + Array will contain 10 elements from 0 - 9
+    + Use Math.random * array.length to generate a random number between 0 and 9, wrap this using Math.floor to round down
+    + Using the generated number, wrap it around [] to access the index
+
+*/
